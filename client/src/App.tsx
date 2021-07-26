557051e5cce092ee66642877abc8cd635549ce9a
Patrick <pat.manser@gmail.com>
2021-07-26 12:09:00 +0200

    initial commit

---
+++ client/src/App.tsx
@@ -0,0 +1,20 @@
+import React from 'react';
+import logo from './logo.svg';
+import './App.css';
+
+const App = () => {
+  return (
+    <div className="p-6 max-w-sm mx-auto bg-white rounded-xl shadow-md flex items-center space-x-4">
+      <div className="flex-shrink-0">
+        bla
+      </div>
+      <div>
+        <div className="text-xl font-medium text-black">Hello</div>
+        <p className="text-gray-500">You have a new message!</p>
+      </div>
+      Penis
+    </div>
+  );
+};
+
+export default App;
